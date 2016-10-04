095dbf10ff83e98ecf275e88c29aa0775fb5e9a1
Alfonsas Cirtautas <alfonsas.cirtautas@callcreditgroup.com>
2016-10-04 11:17:27 +0300

    Extract functionality to hello module

---
+++ HelloWorld/HelloModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class HelloModule
+    {
+        public string SayHello(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return "Name cannot be empty";
+            }
+
+            return string.Format("Hello {0}!", name);
+        }
+    }
+}
